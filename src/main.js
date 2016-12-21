a8aad5c6919bf7913ad3359a1ac63a6695c16ecc
Kayran dos Santos <kayransantos2@gmail.com>
2016-12-20 23:07:03 -0200

    Add husky

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const a = 3;
+const b = 1;
+const c = 'dfkjasdklfalskdfjsdfljsadfjlsandfjhsdjlfhdshflksahdfksjhdlkfjasldkjflksadjflkasdjflkasdjflksajdfkljasdklfjsakdlfjldjflsajdflkasjdfkljsadflkjsdfkljfdklasdf';
+
+function s() {
+  return a + b + c
+}
+
+s();
